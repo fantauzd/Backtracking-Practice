2027d3e3d383aca34d1e08b9e48c924ef4b4956b
fantauzd <fantauzd@oregonstate.edu>
2024-04-27 20:34:41 -0400

    permutations problem solved

--- stringPermutations.py
+++ stringPermutations.py
@@ -1,3 +1,23 @@
 # Problem description: You are given a string of characters.
 # You have to print all possible combinations of those characters (all permutations).
 # You should not repeat any character. Assume that you will be given distinct characters in the string.
+
+def permutations(result, str):
+    #base Case, print the result when we obtain the result using all characters
+    if(len(result) == len(str)):
+        print(''.join(result))
+
+    for i in range(len(str)):
+        current_choice = str[i]
+        # If the choice was not already made we chose it to include in our result
+        if(current_choice not in result):
+            result.append(current_choice)
+            #recursively calling permutations function until we obtain our result
+            permutations(result, str)
+            #Once we have exhausted all possible paths we backtrack
+            result.pop()
+
+def permuations_backtracking(str):
+    permutations([],str)
+
+permuations_backtracking("ABC")
