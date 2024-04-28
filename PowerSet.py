10e926ee3cad501b3b9eb45bdfea79f53767e9d4
fantauzd <fantauzd@oregonstate.edu>
2024-04-28 00:35:15 -0400

    added comments and better naming for powerset

--- PowerSet.py
+++ PowerSet.py
@@ -1,22 +1,39 @@
 # Given a set of n distinct numbers return its power set.
 import copy
 
-def powerset_backtracking(inputSet):
+def powerset(inputSet):
+    """
+    Given a set of n distinct numbers return its power set.
+    :param inputSet: an array of distinct numbers
+    :return: an array that is the power set of inputSet
+    """
+    # store results in an array
     result = []
-    powerset(len(inputSet)-1, [], inputSet, result)
+    # initialize pointer to last index of the inputSet
+    powerset_helper(len(inputSet)-1, [], inputSet, result)
     return result
-def powerset(pointer, choices_made, inputSet, result):
+
+def powerset_helper(pointer, choices_made, inputSet, result):
+    """
+    Helper function for powerset.
+    :param pointer: A pointer for the number we are examining
+    :param choices_made: an array of the numbers that we are considering
+    :param inputSet: an array of numbers
+    :param result: an array representing the power set of inputSet
+    :return: None, modifying result in powerset()
+    """
+    # once we have decided to consider or not consider each element, the remaining combination is a result
     if pointer < 0:
         result.append(copy.deepcopy(choices_made))
         return
 
-    choices_made.append(inputSet[pointer])
-    powerset(pointer-1, choices_made, inputSet, result)
-
-    choices_made.pop()
-    powerset(pointer-1, choices_made, inputSet, result)
+    # at each number we can either consider the number or not consider the number
+    choices_made.append(inputSet[pointer]) # consider the number
+    powerset_helper(pointer-1, choices_made, inputSet, result)
+    choices_made.pop() # do not consider the number
+    powerset_helper(pointer-1, choices_made, inputSet, result)
 
 
 
 if __name__ == "__main__":
-    print(powerset_backtracking("ABC"))
+    print(powerset([1, 2, 3]))
