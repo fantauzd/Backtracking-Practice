1fcc2c695901631062a4fe6100b2098a3ff5421c
fantauzd <fantauzd@oregonstate.edu>
2024-04-27 20:23:30 -0400

    first commit, set up problem

---
+++ stringPermutations.py
@@ -0,0 +1,3 @@
+# Problem description: You are given a string of characters.
+# You have to print all possible combinations of those characters (all permutations).
+# You should not repeat any character. Assume that you will be given distinct characters in the string.
