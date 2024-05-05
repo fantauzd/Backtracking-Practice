2158c2a20d5dc9051f7247989e7cef107ce6eeb9
fantauzd <fantauzd@oregonstate.edu>
2024-05-04 21:37:23 -0400

    complete rework of  combination sum problem

--- CombinationSum.py
+++ CombinationSum.py
@@ -2,25 +2,26 @@
  # find all unique combinations of integers from the nums[] array whose sum is equal to k.
  # Any integer in the array can be chosen an unlimited number of times.
 
+from copy import deepcopy
 def combination_sum(nums, target):
-    combination_sum_helper(nums, target, pointer=0, combination=[], result=[])
+    result = []
+    combination_sum_helper(nums, target, result, pointer=0, combination=[])
+    return result
 
-def combination_sum_helper(nums, target, pointer, combination, result):
+def combination_sum_helper(nums, target, result, pointer, combination):
+    # store valid solutions
+    if target == 0:
+        result.append(deepcopy(combination))
+        return
+    # if we went over target, backtrack
+    if target < 0:
+        return
     # attempt to use each number in a combination, starting with smallest
     for i in range(pointer, len(nums)):
         # try each value in the combination
         combination.append(nums[i])
-        target -= nums[i]
-        # if we have not reached the target, add another number
-        if target > 0:
-            combination_sum_helper(nums, target, pointer, combination, result)
-        # store valid solutions
-        if target == 0:
-            result.append(combination)
-            return
-        # if we went over target, backtrack
-        if target < 0:
-            combination.pop()
-            target += nums[i]
-            combination_sum_helper(nums, target, pointer+1, combination, result)
+        combination_sum_helper(nums, target - nums[i], result, i, combination)
+        combination.pop()
 
+if __name__ == "__main__":
+    print(combination_sum([2, 3, 6, 7], 7))
