afec5494469879ef192cc8723e7770849e2eef05
fantauzd <fantauzd@oregonstate.edu>
2024-05-04 21:30:18 -0400

    solved combination sum problem

--- CombinationSum.py
+++ CombinationSum.py
@@ -2,3 +2,25 @@
  # find all unique combinations of integers from the nums[] array whose sum is equal to k.
  # Any integer in the array can be chosen an unlimited number of times.
 
+def combination_sum(nums, target):
+    combination_sum_helper(nums, target, pointer=0, combination=[], result=[])
+
+def combination_sum_helper(nums, target, pointer, combination, result):
+    # attempt to use each number in a combination, starting with smallest
+    for i in range(pointer, len(nums)):
+        # try each value in the combination
+        combination.append(nums[i])
+        target -= nums[i]
+        # if we have not reached the target, add another number
+        if target > 0:
+            combination_sum_helper(nums, target, pointer, combination, result)
+        # store valid solutions
+        if target == 0:
+            result.append(combination)
+            return
+        # if we went over target, backtrack
+        if target < 0:
+            combination.pop()
+            target += nums[i]
+            combination_sum_helper(nums, target, pointer+1, combination, result)
+
